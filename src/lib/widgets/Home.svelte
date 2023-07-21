72d50321d1abd46f3c042e69d8113d02e58a17e4
Brady Underwood <underwood.brady@gmail.com>
2023-07-21 17:36:41 -0600

    added remaining widgets

---
+++ src/lib/widgets/Home.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import ResponsiveWidget from '$lib/ResponsiveWidget.svelte';
+</script>
+
+<ResponsiveWidget>
+    <section slot="header" class="">
+
+    </section>
+    <div slot="body" class="">
+
+    </div>
+    <section slot="footer">
+        
+    </section>
+</ResponsiveWidget>
---
+++ src/lib/widgets/News.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import ResponsiveWidget from '$lib/ResponsiveWidget.svelte';
+</script>
+
+<ResponsiveWidget>
+    <section slot="header" class="">
+
+    </section>
+    <div slot="body" class="">
+
+    
+    </div>
+</ResponsiveWidget>
